45dc8160b7ffda06c504ccbc3110099aacc8e450
skylaryoshimitsu-blip <skylaryoshimitsu@gmail.com>
2026-06-13 21:11:20 +0800

    Refactor handler initialization for inngest

--- api/inngest.js
+++ api/inngest.js
@@ -1,15 +1,17 @@
 // api/inngest.js
-import { serve } from 'inngest/express';
 import { inngest } from '../inngest/client.js';
 import { scoreAncillaryCall } from '../inngest/functions/scoreAncillaryCall.js';
- 
+
 export const config = { api: { bodyParser: false } };
- 
-const handler = serve({
-  client: inngest,
-  functions: [scoreAncillaryCall],
-});
- 
+
+async function getHandler() {
+  const { serve } = await import('inngest/next');
+  return serve({ client: inngest, functions: [scoreAncillaryCall] });
+}
+
+let handler = null;
+
 export default async function (req, res) {
+  if (!handler) handler = await getHandler();
   return handler(req, res);
 }
