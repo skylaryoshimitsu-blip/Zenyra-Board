cd1cdcb0f7d9103c30b9b65c4dbba9e8e5496cee
skylaryoshimitsu-blip <skylaryoshimitsu@gmail.com>
2026-06-13 20:41:45 +0800

    Refactor Inngest handler implementation
    Refactor Inngest handler to use serve directly from inngest/express.

--- api/inngest.js
+++ api/inngest.js
@@ -1,18 +1,15 @@
 // api/inngest.js
-// Inngest serve handler for plain Vercel serverless (not Next.js)
-
+import { serve } from 'inngest/express';
 import { inngest } from '../inngest/client.js';
 import { scoreAncillaryCall } from '../inngest/functions/scoreAncillaryCall.js';
-
+ 
 export const config = { api: { bodyParser: false } };
-
-let handler = null;
-async function getHandler() {
-  const { serve } = await import('inngest/next');
-  return serve({ client: inngest, functions: [scoreAncillaryCall] });
-}
-
+ 
+const handler = serve({
+  client: inngest,
+  functions: [scoreAncillaryCall],
+});
+ 
 export default async function (req, res) {
-  if (!handler) handler = await getHandler();
   return handler(req, res);
 }
